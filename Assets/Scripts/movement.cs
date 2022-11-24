2594249bf5c3e6f7a631cbcc623505885b16f29a
Dibaa23 <aquraishi7@gatech.edu>
2022-11-24 17:03:45 -0500

    Finished Mouse (Movement and Sprite)

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class movement : MonoBehaviour
 {
     public Rigidbody2D rb2D;
-    private float speed = 1f;
+    private float speed = 8f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,16 +16,18 @@ public class movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb2D.AddForce(transform.right * speed * Time.deltaTime, ForceMode2D.Impulse);
+        rb2D.AddForce(transform.up * speed * Time.deltaTime, ForceMode2D.Impulse);
 
-        if (Input.GetButton("Horizontal") && Input.GetAxisRaw("Horizontal") < 0)
-        {
+        if (Input.GetButton("Horizontal") && Input.GetAxisRaw("Horizontal") < 0) {
             transform.Rotate(0f, 0f, 1f);
         }
 
-        if (Input.GetButton("Horizontal") && Input.GetAxisRaw("Horizontal") > 0)
-        {
+        if (Input.GetButton("Horizontal") && Input.GetAxisRaw("Horizontal") > 0) {
             transform.Rotate(0f, 0f, -1f);
+        }
+
+        else {
+            transform.Rotate(0f, 0f, Random.Range(-0.5f, 0.5f));
         }
     }
 }
