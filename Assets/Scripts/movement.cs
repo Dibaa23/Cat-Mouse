182bd7f987b8621614be95f9818a033f649ce37d
Dibaa23 <aquraishi7@gatech.edu>
2022-12-09 15:34:42 -0500

    Added Post Processing

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class movement : MonoBehaviour
 {
     public Rigidbody2D rb2D;
+    public ParticleSystem dust;
+    private bool boost = false;
     private float speed = 10f;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -16,18 +19,44 @@ public class movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb2D.AddForce(transform.up * speed * Time.deltaTime, ForceMode2D.Impulse);
+        rotation();
+        thrust();
+    }
 
-        if (Input.GetButton("Horizontal") && Input.GetAxisRaw("Horizontal") < 0) {
+
+    public void rotation() {
+        if (Input.GetButton("Horizontal") && Input.GetAxisRaw("Horizontal") < 0)
+        {
             transform.Rotate(0f, 0f, 1f);
         }
 
-        if (Input.GetButton("Horizontal") && Input.GetAxisRaw("Horizontal") > 0) {
+        if (Input.GetButton("Horizontal") && Input.GetAxisRaw("Horizontal") > 0)
+        {
             transform.Rotate(0f, 0f, -1f);
         }
 
-        else {
+        else
+        {
             transform.Rotate(0f, 0f, Random.Range(-0.5f, 0.5f));
+        }
+    }
+
+    public void thrust() {
+        rb2D.AddForce(transform.up * speed * Time.deltaTime, ForceMode2D.Impulse);
+
+        if (Input.GetKeyDown("w"))
+        {
+            dust.Play();
+            boost = true;
+        }
+
+        if (Input.GetKey("w") && boost)
+        {
+            speed = 20f;
+        }
+
+        else {
+            speed = 10f;
         }
 
     }
