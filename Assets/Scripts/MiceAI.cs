95c9a2bc907249a9cc5cf86af27d8e43ef643177
Dibaa23 <aquraishi7@gatech.edu>
2022-12-11 22:53:17 -0500

    .

--- Assets/Scripts/MiceAI.cs
+++ Assets/Scripts/MiceAI.cs
@@ -90,7 +90,7 @@ public class MiceAI : MonoBehaviour
 
     public void Flee()
     {
-        if (Vector2.Distance(Cat.transform.position, transform.position) <= 5f && (!attack))
+        if (Vector2.Distance(Cat.transform.position, transform.position) <= 10f && (!attack))
         {
             runAway = true;
             transform.Rotate(0f, 0f, Random.Range(0f, 22.5f));
