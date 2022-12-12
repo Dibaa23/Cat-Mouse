46c840493925c28ef922d3b5e065b209ca9afc10
Dibaa23 <aquraishi7@gatech.edu>
2022-12-12 00:05:34 -0500

    update

--- Assets/Scripts/MiceAI.cs
+++ Assets/Scripts/MiceAI.cs
@@ -13,8 +13,10 @@ public class MiceAI : MonoBehaviour
     private float HP;
     private float rotateRate1;
     private float rotateRate2;
+    private float chargetime;
+    private bool swarm;
+    private bool swarmReady;
     private bool runAway;
-    private bool attack;
     private bool boundrybreach;
 
 
@@ -28,10 +30,14 @@ public class MiceAI : MonoBehaviour
         transform.localScale = new Vector2(size, size);
         HP =  size / 2f;
         runAway = false;
-        attack = false;
+        swarm = false;
         rotateRate1 = Random.Range(0.25f, 0.50f);
         rotateRate2 = Random.Range(2.5f, 5.0f);
         boundrybreach = false;
+        swarm = false;
+        swarmReady = true;
+        chargetime = 5f;
+        Debug.Log("Swarm : Ready");
         transform.localRotation = Quaternion.Euler(0, 0, Random.Range(0f, 360f));
         transform.position = new Vector3(Random.Range(-26f, 26f), Random.Range(-17f, 17f), transform.position.z);
         InvokeRepeating("RotateSmall", 1f, rotateRate1);
@@ -44,10 +50,17 @@ public class MiceAI : MonoBehaviour
         thrust();
         Breach();
         Flee();
+        //Swarm();
 
         if (HP <= 0f)
         {
             Destroy(gameObject);
+        }
+
+        else if (Input.GetKeyDown("n") && swarmReady)
+        {
+            swarm = true;
+            StartCoroutine(Normalize2(5));
         }
 
     }
@@ -90,7 +103,7 @@ public class MiceAI : MonoBehaviour
 
     public void Flee()
     {
-        if (Vector2.Distance(Cat.transform.position, transform.position) <= 10f && (!attack))
+        if (Vector2.Distance(Cat.transform.position, transform.position) <= 5f && (!swarm))
         {
             runAway = true;
             transform.Rotate(0f, 0f, Random.Range(0f, 22.5f));
@@ -115,5 +128,29 @@ public class MiceAI : MonoBehaviour
     {
         yield return new WaitForSeconds(waitTime);
         boundrybreach = false;
+    }
+
+    IEnumerator Normalize2(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        swarm = false;
+        Debug.Log("Swarm : Not Ready");
+        yield return new WaitForSeconds(chargetime);
+        swarmReady = true;
+        Debug.Log("Ready");
+    }
+
+    public void Swarm()
+    {
+        if (Vector2.Distance(Cat.transform.position, transform.position) <= 10f && swarm)
+        {
+            Debug.Log("Swarm : In Progress");
+            transform.position = Vector2.MoveTowards(transform.position, Cat.transform.position, (2 * speed) * Time.deltaTime);
+            Vector2 direction = (Vector2)Cat.transform.position - (Vector2)transform.position;
+            direction.Normalize();
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(Vector3.forward * (angle));
+            swarmReady = false;
+        }
     }
 }
